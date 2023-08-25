57fd976180182de3d6dc2207d16a437164d50ca2
saikrishna7783 <saikrishna7783@outlook.com>
2023-08-25 08:15:43 +0530

    First Commit

--- MergeSort.c
+++ MergeSort.c
@@ -70,12 +70,10 @@ int main(void)
     // printf("Enter the elements: ");
     // for (int i = 0; i < n; i++)
     //     scanf("%d", &arr[i]);
-    
+
     srand(time(0));
     for (int i = 0; i < n; i++)
-    {
         arr[i] = rand();
-    }
 
     clock_t st, end;
     st = clock();
